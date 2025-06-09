142e898c5524a0fe0801b8306c8dd3d00139b728
Shogo Takata <peshogo@gmail.com>
2025-06-09 13:51:04 +0900

    fix sm

--- keystone-sm/package.nix
+++ keystone-sm/package.nix
@@ -1,43 +1,45 @@
 {
   opensbi_1_1,
-  runCommand,
   stdenv,
   python3,
   keystone,
 }:
-let
-  keystone-sm-patched = runCommand "keystone" { } ''
-    cp -r ${keystone.src} $out
-    substituteInPlace $out/sm/src/thread.{h,c} \
-      --replace-warn sbadaddr stval
-  '';
-in
 stdenv.mkDerivation {
   pname = "opensbi-kestone-sm";
   inherit (opensbi_1_1) version;
   srcs = [
     (opensbi_1_1.src.overrideAttrs { name = "opensbi"; })
-    keystone-sm-patched
+    (builtins.path {
+      name = "keystone";
+      path = keystone.src;
+    })
   ];
   nativeBuildInputs = [ python3 ];
   enableParallelBuilding = true;
   sourceRoot = ".";
   makeFlags = [
     "O=$(out)"
+    "KEYSTONE_PLATFORM=generic"
+    "KEYSTONE_SDK_DIR=${keystone.sdk}"
+    "PLATFORM=generic"
+    "PLATFORM_RISCV_ABI=lp64d"
+    "PLATFORM_RISCV_ISA=rv64imafd_zifencei_zicsr"
+    "PLATFORM_RISCV_TOOLCHAIN_DEFAULT=1"
   ];
   postPatch = ''
     patchShebangs ./opensbi/scripts
-      (cd opensbi; patch --verbose -p1 < ${./opensbi-change-basename.patch}; patch --verbose -p1 < ${./opensbi-firmware-secure-boot.patch})
-      makeFlagsArray+=(
-        "-C"
-        "$(pwd)/opensbi"  
-        "PLATFORM_DIR=$(pwd)/keystone/sm/plat/generic"
-        "KEYSTONE_SM=$(pwd)/keystone/sm"
-        "KEYSTONE_SDK_DIR=$(pwd)/keystone/sdk"
-      )
+    substituteInPlace ./keystone/sm/src/thread.{h,c} \
+      --replace-warn sbadaddr stval
+    (
+      cd opensbi
+      patch --verbose -p1 < ${./opensbi-change-basename.patch}
+      patch -p1 < ${./opensbi-firmware-secure-boot.patch}
+    )
+    makeFlagsArray+=(
+      "KEYSTONE_SM=$(pwd)/keystone/sm"
+      "PLATFORM_DIR=$(pwd)/keystone/sm/plat/"
+      "-C"
+      "$(pwd)/opensbi"
+    )
   '';
-
 }
-# opensbi_1_1.overrideAttrs (old: {
-#   postPatch = (old.postPatch or "") + '''';
-# })
