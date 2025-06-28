3fa5d02111c78bf3a21947d309a45c26be2d5952
Shogo Takata <peshogo@gmail.com>
2025-06-28 18:06:56 +0900

    runtime: refactor

--- keystone-runtime/package.nix
+++ keystone-runtime/package.nix
@@ -4,16 +4,23 @@
   cmake,
   lib,
   plugins ? [ ],
+  internalStrace ? false,
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "keystone-runtime";
   version = "0";
   inherit (keystone) src;
 
+  patches = [
+    ./req_pages_define.patch
+    ./norelax.patch
+  ];
+
   nativeBuildInputs = [ cmake ];
 
   cmakeFlags = [
     (lib.cmakeFeature "KEYSTONE_SDK_DIR" "${keystone.sdk}")
+    (lib.cmakeBool "INTERNAL_STRACE" internalStrace)
   ] ++ map (p: lib.cmakeBool (lib.toUpper p) true) plugins;
 
   postPatch = ''
@@ -22,23 +29,27 @@ stdenv.mkDerivation (finalAttrs: {
       --replace-fail sbadaddr stval
   '';
 
-  # preInstall = "ls ..";
   installPhase = ''
     runHook preInstall
 
     cd ..
-    install -Dt $out loader.bin
-    install -Dt $out eyrie-rt
-    install -Dt $out .options_log
-    cp -r include $out
+    install -Dt $out/share loader.bin
+    install -Dt $out/share eyrie-rt
+    install -Dt $out/share .options_log
 
     runHook postInstall
   '';
 
+  stripDebugList = [ "share" ];
+
   hardeningDisable = [ "stackprotector" ];
 
+  makeFlags = ["VERBOSE=1"];
+
+  env.NIX_CFLAGS_COMPILE="-march=rv64g_zifencei_zicsr -mabi=lp64d";
+
   passthru = {
-    loader = "${finalAttrs.finalPackage}/loader.bin";
-    rt = "${finalAttrs.finalPackage}/eyrie-rt";
+    loader = "${finalAttrs.finalPackage}/share/loader.bin";
+    rt = "${finalAttrs.finalPackage}/share/eyrie-rt";
   };
 })
